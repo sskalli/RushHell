c672f844e3731da433f65973d5611efb97ba1942
skalli_s <sami.skalli@epitech.eu>
2016-12-03 17:40:27 +0100

    fdp

---
+++ purgatory/etape1/CMakeFiles/test_fsa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_fsa.dir/Machine.cpp.o"
+  "CMakeFiles/test_fsa.dir/main.cpp.o"
+  "test_fsa.pdb"
+  "test_fsa"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_fsa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ purgatory/etape1/main.cpp
@@ -0,0 +1,28 @@
+#include "Machine.hpp"
+#include <iostream>
+#include <strings.h>
+
+void	moveAppend()
+{
+}
+
+void	haltReset()
+{
+}
+
+void	machine(char *s)
+{
+  char	alph[7] = "mechant";
+  char	token[7];
+
+  for (int i = 0; s[i]; i++)
+    {
+      
+    }
+}
+
+int	main(int, char **av)
+{
+  machine(av[1]);
+  return 1;
+} 
