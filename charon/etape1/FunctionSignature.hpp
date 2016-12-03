bf5b12cd050c9de158b6973d40125d463b10eb68
de-buy_i <ismael.de-buyst@epitech.eu>
2016-12-03 12:21:20 +0100

    bonjour

---
+++ charon/etape1/FunctionSignature.hpp
@@ -0,0 +1,10 @@
+#ifdef __FUNCTIONSIGNATURE_HPP_
+#define __FUNCTIONSIGNATURE_HPP_
+
+template<typename T>
+struct FunctionSignature
+{
+typedef T* type;
+};
+
+#endif __FUNCTIONSIGNATURE_HPP_
---
+++ charon/etape1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "FunctionSignature.hpp"
+
+int main(int ac, char **av)
+{
+    /*
+     * un test avec
+     * une fonction void et 1 param
+     * plusieurs param meme type
+     * plusieurs param différents type
+     * une fonction PAS du meme type que les autres
+     */
+    return 0;
+}
